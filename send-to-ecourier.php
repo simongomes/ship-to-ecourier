2f8883709f03cee7bf6dd4b6057d1dba0c620f8b
Simon Gomes <busy.s.simon@gmail.com>
2021-04-22 20:18:09 +0600

    plugin initial files added

---
+++ send-to-ecourier.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name:     Send To eCourier
+ * Plugin URI:      https://simongomes.dev
+ * Description:     Send To eCourier gives you ability to send your parcel request to eCourier directly from your WooCommerce dashboard, it enables booking automarion from your WordPress website.
+ * Author:          Simon Gomes
+ * Author URI:      https://simongomes.dev
+ * Text Domain:     send-to-ecourier
+ * Domain Path:     /languages
+ * Version:         1.0.1
+ * License:         GPLv3
+ * License URI:     https://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * @package         SendToEcourier
+ */
+
+// Block direct access to the file.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
