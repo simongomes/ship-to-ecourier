8950551c2cf569ed533125ba4f750f83241b0eaa
Simon Gomes <busy.s.simon@gmail.com>
2021-04-29 22:47:56 +0600

    plugin settings link

--- includes/Admin/Menu.php
+++ includes/Admin/Menu.php
@@ -34,6 +34,7 @@ if ( ! class_exists( 'Menu' ) ) {
 			$this->settings = $settings;
 
 			add_action( 'admin_menu', array( $this, 'add_admin_menu' ) );
+			add_filter( 'plugin_action_links_' . plugin_basename( STE_FILE ), array( $this, 'add_settings_menu' ) );
 		}
 
 		/**
@@ -45,6 +46,19 @@ if ( ! class_exists( 'Menu' ) ) {
 			$parent_slug = 'ship-to-ecourier';
 			$capability  = 'manage_options';
 			add_menu_page( __( 'eCourier Settings', 'ship-to-ecourier' ), __( 'eCourier Settings', 'send-to-ecourier' ), $capability, $parent_slug, array( $this->settings, 'load_settings_page' ), STE_ASSETS_URL . '/images/menu_icon.png' );
+		}
+
+		/**
+		 * Add settings menu link to plugin page.
+		 *
+		 * @param array $links plugin links.
+		 *
+		 * @return array
+		 */
+		public function add_settings_menu( array $links ) {
+			$settings_link = '<a href="' . admin_url( 'admin.php?page=ship-to-ecourier' ) . '">' . __( 'Settings', 'ship-to-ecourier' ) . '</a>';
+			array_unshift( $links, $settings_link );
+			return $links;
 		}
 	}
 }
